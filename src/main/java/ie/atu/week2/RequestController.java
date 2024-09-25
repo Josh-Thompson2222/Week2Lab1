688e84144d080f536457651a702d7287768a67bf
Josh Thompson <G00422756@atu.ie>
2024-09-25 15:33:08 +0100

    Added a requestParam annotation

--- src/main/java/ie/atu/week2/RequestController.java
+++ src/main/java/ie/atu/week2/RequestController.java
@@ -1,9 +1,7 @@
 package ie.atu.week2;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+//import
 
 @RestController
 @RequestMapping("/person")
@@ -19,6 +17,9 @@ public class RequestController {
     {
         return "User name is: " + name;
     }
-
-
+    @GetMapping("/details")
+    public String details(@RequestParam String name, @RequestParam int age)
+    {
+        return name + " " + age;
+    }
 }
